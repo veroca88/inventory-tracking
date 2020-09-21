a62124df29f641f823722cd4981ba975457eecc0
veroca88 <veronicadonoso1506@gmail.com>
2020-09-21 17:39:27 -0400

    Create product model

---
+++ models/Product.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = mongoose;
+const productSchema = new Schema(
+  {
+    productName: {
+      type: String,
+      trim: true,
+      required: [true, "Please insert name of product."],
+    },
+    barcode: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: [true, "Please insert price of product."],
+    },
+    description: {
+      type: String,
+    },
+    date: {
+      type: Date,
+      default: [true, "You need to insert a date."],
+    },
+    quantity: {
+      type: Number,
+      trim: true,
+      required: [true, "Please insert quantity of product."],
+      default: 0,
+    },
+    availability: {
+      type: String,
+      enum: ["available", "sold"],
+      default: "available",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "You need an owner id"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Product", productSchema);
