c38e9b83130e32f178765478c1ff27e72926ca36
veroca88 <veronicadonoso1506@gmail.com>
2020-09-23 10:34:07 -0400

    Test with auth routes

---
+++ routes/auth-routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const routeGuard = require("../configs/route-guard.config");
+const User = require("../models/User.model");
+
+router.get("/", (req, res, next) => {
+  console.log("hey :)");
+  res.render("auth-views/signup");
+});
+
+// authRouter.post("/", (req, res) => {
+//   const { firstName, lastName, username, email, password } = req.body;
+
+//   if (
+//     firstName === "" ||
+//     lastName === "" ||
+//     username === "" ||
+//     password === "" ||
+//     email === ""
+//   ) {
+//     res.render("auth-views/signup");
+//   }
+// });
+
+module.exports = authRouter;
